1d8b847d5859703d70e21852c394e629da83c617
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2024-10-17 10:39:23 -0400

    Init Project Structure (#1)
    * add project init files
    
    * create sample tsvs
    
    * set up config file
    
    * finish project structure setup
    
    * remove global reqs, update readme
    
    * update notebook
    
    * update notebook to not print example names
    
    * gitignore newline

---
+++ example_analysis/run_analysis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the path to the main Snakefile and the config file
+SNAKEFILE="../workflow/Snakefile"
+CONFIG_FILE="config/config.yml"
+
+# Run Snakemake with the specified Snakefile and config file
+snakemake --snakefile "$SNAKEFILE" --configfile "$CONFIG_FILE" --cores all

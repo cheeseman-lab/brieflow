61ead82ea4eca33a9033c8eafe3570d467727e6b
Roshan Kern <roshan.kern@ucdenver.edu>
2024-10-18 10:34:25 -0700

    make snakemake accesible

--- workflow/scripts/preprocess/extract_metadata_tile.py
+++ workflow/scripts/preprocess/extract_metadata_tile.py
@@ -1,11 +1,17 @@
 import sys
 import pickle
 import pandas as pd
+from workflow.lib.preprocess.preprocess import _extract_metadata_tile
 
-# Snakemake automatically passes the input and output file paths via snakemake.input and snakemake.output
-input_filepaths = snakemake.input  # This will be the list of file paths passed from the rule
-output_filepath = snakemake.output[0]  # This is the output path specified in the Snakemake rule
+# Load file paths from snakemake input
+input_files = snakemake.input  # This will be the list of ND2 file paths from the Snakemake rule
 
-# Save the list of filepaths to a pickle file
-with open(output_filepath, 'wb') as f:
-    pickle.dump(input_filepaths, f)
+# Extract metadata using the _extract_metadata_tile function
+metadata_df = _extract_metadata_tile(input_files)
+
+# Save the extracted metadata to the output path
+output_path = snakemake.output[0]  # The output file path from the Snakemake rule
+
+# Save metadata as a pickle file
+with open(output_path, 'wb') as f:
+    pickle.dump(metadata_df, f)

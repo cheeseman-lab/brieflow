98ab9964bfe42794f5b878424905bae723d74c8f
Roshan Kern <roshan.kern@ucdenver.edu>
2024-11-02 14:31:27 -0400

    format

--- workflow/scripts/sbs_process/find_peaks.py
+++ workflow/scripts/sbs_process/find_peaks.py
@@ -5,9 +5,7 @@ from skimage.io import imread, imsave
 standard_deviation_data = imread(snakemake.input[0])
 
 # find peaks
-peaks = find_peaks(
-    standard_deviation_data = standard_deviation_data
-)
+peaks = find_peaks(standard_deviation_data=standard_deviation_data)
 
 # save peak data
 imsave(snakemake.output[0], peaks)

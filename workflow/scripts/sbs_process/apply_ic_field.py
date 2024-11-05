813ec53f7195a4144772d90d0eabbecf708b2ce6
Roshan Kern <roshan.kern@ucdenver.edu>
2024-11-05 18:29:34 -0500

    more consistent name

--- workflow/scripts/sbs_process/apply_ic_field.py
+++ workflow/scripts/sbs_process/apply_ic_field.py
@@ -3,13 +3,17 @@ from skimage.io import imread, imsave
 
 # load aligned image data
 aligned_image_data = imread(snakemake.input[0])
-aligned_segmentation_image = aligned_image_data[snakemake.params.segmentation_cycle - 1]
+aligned_image_data_segmentation_cycle = aligned_image_data[
+    snakemake.params.segmentation_cycle - 1
+]
 
 # load illumination correction field
 ic_field = imread(snakemake.input[1])
 
 # apply illumination correction field
-corrected_image_data = apply_ic_field(aligned_segmentation_image, correction=ic_field)
+corrected_image_data = apply_ic_field(
+    aligned_image_data_segmentation_cycle, correction=ic_field
+)
 
 # save corrected image data
 imsave(snakemake.output[0], corrected_image_data)

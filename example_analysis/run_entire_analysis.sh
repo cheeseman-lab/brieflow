e2102f626a0143ac959687f6e8bee740c4839ac4
Roshan Kern <roshan.kern@ucdenver.edu>
2024-11-19 19:08:31 -0500

    don't create rulegraph

--- example_analysis/run_entire_analysis.sh
+++ example_analysis/run_entire_analysis.sh
@@ -7,7 +7,7 @@ RULEGRAPH_FILE="../images/brieflow_rulegraph.png"
 
 # Generate a rulegraph of the Snakefile
 # NOTE: Uncomment when needed, takes extra computation
-snakemake --snakefile "$SNAKEFILE" --configfile "$CONFIG_FILE" --rulegraph | dot -Gdpi=100 -Tpng -o "$RULEGRAPH_FILE"
+# snakemake --snakefile "$SNAKEFILE" --configfile "$CONFIG_FILE" --rulegraph | dot -Gdpi=100 -Tpng -o "$RULEGRAPH_FILE"
 
 # Run Snakemake with the specified Snakefile and config file
 snakemake  --use-conda --cores all --snakefile "$SNAKEFILE" --configfile "$CONFIG_FILE"

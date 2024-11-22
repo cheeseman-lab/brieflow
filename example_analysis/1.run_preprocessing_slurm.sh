8be519c8c7f76fbf06ad4e74592b2f22083399f3
mdiberna <mdiberna@fry.wi.mit.edu>
2024-11-22 17:32:22 -0500

    first working version of slurm integration

--- example_analysis/1.run_preprocessing_slurm.sh
+++ example_analysis/1.run_preprocessing_slurm.sh
@@ -5,6 +5,9 @@ BRIEFLOW_PATH="../"
 CONFIG_FILE_PATH="config/config.yml"
 PROFILE_FILE_PATH="slurm/"
 
+# Make a directory for the slurm output files
+mkdir -p slurm_output/stdout slurm_output/stderr
+
 # Run only the preprocess rules with cluster configuration
 snakemake --use-conda --executor slurm \
     --workflow-profile "$PROFILE_FILE_PATH" \

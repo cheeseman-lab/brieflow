ed1f44d5a366836ba3b598344fe25167cbddfc3c
mdiberna <mdiberna@fry.wi.mit.edu>
2024-11-22 16:33:23 -0500

    getting closer

--- example_analysis/1.run_preprocessing_slurm.sh
+++ example_analysis/1.run_preprocessing_slurm.sh
@@ -3,11 +3,11 @@
 # Set the path to the main Snakefile and the config file
 BRIEFLOW_PATH="../"
 CONFIG_FILE_PATH="config/config.yml"
-PROFILE_FILE_PATH="config/"
+PROFILE_FILE_PATH="slurm/"
 
 # Run only the preprocess rules with cluster configuration
-snakemake --use-conda --cores all \
-    --profile "$PROFILE_FILE_PATH" \
+snakemake --use-conda --executor slurm \
+    --workflow-profile "$PROFILE_FILE_PATH" \
     --snakefile "${BRIEFLOW_PATH}workflow/Snakefile" \
     --configfile "$CONFIG_FILE_PATH" \
     --until all_preprocess

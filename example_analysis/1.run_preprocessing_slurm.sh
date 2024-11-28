958ef16d4ff2b2b16183b6190cacdc8ea7acce1f
Matteo Di Bernardo <53015428+mat10d@users.noreply.github.com>
2024-11-28 07:58:42 -0700

    Adding HPC capabilites to brieflow (#18)
    * trying to get hpc / slurm integration setup, for Andy
    
    * getting closer
    
    * first working version of slurm integration
    
    * added complete config, further steps
    
    * cleaned up outputs
    
    * changed slurm interlocutor to conda installable (works) and other formatting changes
    
    ---------
    
    Co-authored-by: mdiberna <mdiberna@fry.wi.mit.edu>

---
+++ example_analysis/1.run_preprocessing_slurm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=preprocessing   # Job name
+#SBATCH --partition=20                   # Partition name
+#SBATCH --ntasks=1                       # Run a single task
+#SBATCH --cpus-per-task=1               # Single CPU for the controller job
+#SBATCH --mem=4G                        # Memory for the controller job
+#SBATCH --time=72:00:00                 # Time limit (hrs:min:sec)
+#SBATCH --output=slurm_output/main/preprocessing-%j.out  # Standard output log
+
+# Set the path to the main Snakefile, the config file, and the workflow profile
+BRIEFLOW_PATH="../"
+CONFIG_FILE_PATH="config/config.yml"
+PROFILE_FILE_PATH="slurm/"
+
+# Create slurm output directories
+mkdir -p slurm_output/rule
+
+# Activate conda environment (adjust path as needed)
+source ~/.bashrc
+conda activate brieflow_workflows
+
+# Run Snakemake
+snakemake --use-conda --executor slurm \
+    --workflow-profile "$PROFILE_FILE_PATH" \
+    --snakefile "${BRIEFLOW_PATH}workflow/Snakefile" \
+    --configfile "$CONFIG_FILE_PATH" \
+    --until all_preprocess
+    

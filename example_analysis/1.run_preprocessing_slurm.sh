842c6a421684e2599f9e7a6a8229564a78c9c629
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2024-12-13 09:11:42 -0500

    Clean Example Analysis (#27)
    * update example analysis
    
    * rerun sbs and preprocess nbs

--- example_analysis/1.run_preprocessing_slurm.sh
+++ example_analysis/1.run_preprocessing_slurm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --job-name=preprocessing   # Job name
 #SBATCH --partition=20                   # Partition name
 #SBATCH --ntasks=1                       # Run a single task
@@ -7,11 +8,6 @@
 #SBATCH --time=72:00:00                 # Time limit (hrs:min:sec)
 #SBATCH --output=slurm_output/main/preprocessing-%j.out  # Standard output log
 
-# Set the path to the main Snakefile, the config file, and the workflow profile
-BRIEFLOW_PATH="../"
-CONFIG_FILE_PATH="config/config.yml"
-PROFILE_FILE_PATH="slurm/"
-
 # Create slurm output directories
 mkdir -p slurm_output/rule
 
@@ -19,11 +15,10 @@ mkdir -p slurm_output/rule
 source ~/.bashrc
 conda activate brieflow_workflows
 
-# Run Snakemake
+# Run Snakemake for preprocess rules
 snakemake --executor slurm --use-conda \
-    --workflow-profile "$PROFILE_FILE_PATH" \
-    --snakefile "${BRIEFLOW_PATH}workflow/Snakefile" \
-    --configfile "$CONFIG_FILE_PATH" \
+    --workflow-profile "slurm/" \
+    --snakefile "../workflow/Snakefile" \
+    --configfile "config/config.yml" \
     --latency-wait 60 \
     --until all_preprocess
-    

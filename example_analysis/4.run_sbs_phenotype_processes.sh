ae915828eeb0b2e7a4998feab0b80365ae49d8ee
Roshan Kern <roshan.kern@ucdenver.edu>
2024-12-13 08:57:32 -0500

    update example analysis

--- example_analysis/4.run_sbs_phenotype_processes.sh
+++ example_analysis/4.run_sbs_phenotype_processes.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Set the path to the main Snakefile and the config file
-BRIEFLOW_PATH="../"
-CONFIG_FILE_PATH="config/config.yml"
-
-# TODO: Run both processes when done with testing
-
 # Run the SBS/phenotype process rules
 snakemake --use-conda --cores all \
-    --snakefile "${BRIEFLOW_PATH}workflow/Snakefile" \
-    --configfile "$CONFIG_FILE_PATH" \
+    --snakefile "../workflow/Snakefile" \
+    --configfile "config/config.yml" \
     --until all_sbs_process all_phenotype_process
--- example_analysis/4.run_sbs_phenotype_processes_slurm.sh
+++ example_analysis/4.run_sbs_phenotype_processes_slurm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --job-name=sbs_phenotype_process   # Job name
 #SBATCH --partition=20                   # Partition name
 #SBATCH --ntasks=1                       # Run a single task
@@ -7,11 +8,6 @@
 #SBATCH --time=72:00:00                 # Time limit (hrs:min:sec)
 #SBATCH --output=slurm_output/main/sbs_phenotype_process-%j.out  # Standard output log
 
-# Set the path to the main Snakefile, the config file, and the workflow profile
-BRIEFLOW_PATH="../"
-CONFIG_FILE_PATH="config/config.yml"
-PROFILE_FILE_PATH="slurm/"
-
 # Create slurm output directories
 mkdir -p slurm_output/rule
 
@@ -19,12 +15,17 @@ mkdir -p slurm_output/rule
 source ~/.bashrc
 conda activate brieflow_workflows
 
+# Unlock
+snakemake --unlock --use-conda \
+    --workflow-profile "slurm/" \
+    --snakefile "../workflow/Snakefile" \
+    --configfile "config/config.yml" \
+    --latency-wait 60
+
 # Run only the preprocess rules
 snakemake --executor slurm --use-conda \
-    --workflow-profile "$PROFILE_FILE_PATH" \
-    --snakefile "${BRIEFLOW_PATH}workflow/Snakefile" \
-    --configfile "$CONFIG_FILE_PATH" \
+    --workflow-profile "slurm/" \
+    --snakefile "../workflow/Snakefile" \
+    --configfile "config/config.yml" \
     --latency-wait 60 \
     --until all_sbs_process all_phenotype_process
-
-    

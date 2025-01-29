cf1e09e8c20e9b2e5f7d250decf4bb028b9f3002
Roshan Kern <rsk119@case.edu>
2025-01-29 17:06:07 -0500

    add generate_montage_script

--- workflow/scripts/aggregate_process/generate_montage.py
+++ workflow/scripts/aggregate_process/generate_montage.py
@@ -1,3 +1,27 @@
 import pandas as pd
 
-print(snakemake.input)
+from lib.aggregate.montage_utils import create_cell_montage
+
+print("Generating montage for gene: ", snakemake.wildcards.gene)
+
+# read cell data
+cell_data = pd.read_hdf(input[0])
+
+# subset data to inly include target gene and sgrna combination
+cell_data = cell_data[
+    (cell_data["gene"] == snakemake.wildcards.gene)
+    & (cell_data["sgrna"] == snakemake.wildcards.sgrna)
+]
+
+montage = create_cell_montage(cell_data, snakemake.params.channels)
+
+if montage is not None:
+    save(
+        output[0],
+        montage,
+        display_mode="grayscale",
+        display_ranges=DISPLAY_RANGES[wildcards.channel],
+    )
+else:
+    # Create empty file if no cells found
+    open(output[0], "w").close()

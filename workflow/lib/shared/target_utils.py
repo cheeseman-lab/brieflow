a9782c89ea1b35998abb4589a18de31dde4197b8
Roshan Kern <rsk119@case.edu>
2025-02-14 14:20:40 -0500

    merge testing

--- workflow/lib/shared/target_utils.py
+++ workflow/lib/shared/target_utils.py
@@ -98,5 +98,6 @@ def output_to_input(output_path, wildcard_values, wildcards, ancient_output=Fals
     # Prevent rerunning if ancient
     if ancient_output:
         inputs = [ancient(path) for path in inputs]
+    print(inputs)
 
     return inputs

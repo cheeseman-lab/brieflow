044152cc5ec6eefaae4d7bb6e50e90e11231bbb9
Roshan Kern <rsk119@case.edu>
2025-03-03 12:52:11 -0500

    remove example analysis folder

--- example_analysis/10.run_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run cluster rules
-snakemake --use-conda --cores all \
-    --snakefile "../workflow/Snakefile" \
-    --configfile "config/config.yml" \
-    --rerun-triggers mtime \
-    --until all_cluster

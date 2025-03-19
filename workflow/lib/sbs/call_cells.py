818b2cb2a5b9b7733b498eecfe0a28ce00e0dcd2
mdiberna <mdiberna@fry.wi.mit.edu>
2025-03-19 18:35:34 -0400

    add optional UMI calling to call_cells

--- workflow/lib/sbs/call_cells.py
+++ workflow/lib/sbs/call_cells.py
@@ -20,17 +20,27 @@ from lib.sbs.constants import (
     BARCODE_COUNT_1,
     POSITION_I,
     POSITION_J,
+    UMI_0,
+    UMI_1,
+    UMI_COUNT,
+    UMI_COUNT_0,
+    UMI_COUNT_1,
 )
 
 
-def call_cells(reads_data, df_pool=None, q_min=0):
-    """Perform median correction independently for each tile.
+def call_cells(reads_data, df_pool=None, q_min=0, barcode_col='sgRNA', df_UMI=None):
+    """Process sequencing reads to identify cell barcodes, optionally mapping them to a pool design.
+
+    This function takes sequencing read data and identifies the most frequent barcodes for each cell.
+    If a pool design is provided, it will map the barcodes to the corresponding guide RNAs and gene information.
 
     Args:
         reads_data (DataFrame): DataFrame containing read information.
         df_pool (DataFrame, optional): DataFrame containing pool information. Default is None.
         q_min (int, optional): Minimum quality threshold. Default is 0.
-
+        barcode_col (str, optional): Column in df_pool with barcodes. Default is 'sgRNA' (e.g. CROPseq)
+        df_UMI (DataFrame, optional): DataFrame containing UMI reads. Default is None.
+        
     Returns:
         DataFrame: DataFrame containing corrected cells.
     """
@@ -66,20 +76,28 @@ def call_cells(reads_data, df_pool=None, q_min=0):
             "gene_id_1",
         ]
         return pd.DataFrame(columns=columns)
-
+    
+    
+    # Columns for grouping
+    cols = [WELL, TILE, CELL]
+    
     # Check if df_pool is None
     if df_pool is None:
         # Filter reads by quality threshold and call cells
-        return reads_data.query("Q_min >= @q_min").pipe(call_cells_helper)
+        df_cells = reads_data.query("Q_min >= @q_min").pipe(call_cells_helper)
     else:
         # Determine the experimental prefix length
         prefix_length = len(reads_data.iloc[0].barcode)
-
         # Add prefix to the pool DataFrame
-        df_pool[PREFIX] = df_pool.apply(lambda x: x.sgRNA[:prefix_length], axis=1)
-
+        df_pool[PREFIX] = df_pool.apply(lambda x: x[barcode_col][:prefix_length], axis=1)
         # Filter reads by quality threshold and call cells mapping
-        return reads_data.query("Q_min >= @q_min").pipe(call_cells_mapping, df_pool)
+        df_cells = reads_data.query("Q_min >= @q_min").pipe(call_cells_mapping, df_pool)
+    
+    # If UMI data is provided, add UMI information to the cell data
+    if df_UMI is not None:
+        return call_cells_add_UMIs(df_cells, df_UMI, cols=cols)
+    
+    return df_cells
 
 
 def call_cells_helper(df_reads):
@@ -247,3 +265,68 @@ def call_cells_mapping(df_reads, df_pool):
     )
 
     return df_cells
+
+
+def call_cells_add_UMIs(df_cells, df_UMI, cols=[WELL, TILE, CELL]):
+    """Add UMI (Unique Molecular Identifier) information to called cells.
+    
+    Args:
+        df_cells (DataFrame): DataFrame containing called cells.
+        df_UMI (DataFrame): DataFrame containing UMI reads.
+        cols (list, optional): List of columns to use to merge DataFrames. Default is [WELL, TILE, CELL].
+        
+    Returns:
+        DataFrame: df_cells DataFrame with top UMI counts.
+    """
+    s = (
+        df_UMI.drop_duplicates([WELL, TILE, READ])  # Drop duplicate reads
+        .groupby(cols)[BARCODE]  # Group by well, tile, and cell, and barcode
+        .value_counts()  # Count occurrences of each barcode within each group
+        .rename("count")  # Rename the resulting series to 'count'
+        .sort_values(ascending=False)  # Sort in descending order
+        .reset_index()  # Reset the index
+        .groupby(cols)  # Group again by well, tile, and cell
+    )
+    
+    df_cells_UMI = (
+        df_UMI.join(
+            s.nth(0)[["well", "tile", "cell", "barcode"]]
+            .rename(columns={"barcode": UMI_0})
+            .set_index(cols),
+            on=cols,
+        )
+        .join(
+            s.nth(0)[["well", "tile", "cell", "count"]]
+            .rename(columns={"count": UMI_COUNT_0})
+            .set_index(cols),
+            on=cols,
+        )
+        .join(
+            s.nth(1)[["well", "tile", "cell", "barcode"]]
+            .rename(columns={"barcode": UMI_1})
+            .set_index(cols),
+            on=cols,
+        )
+        .join(
+            s.nth(1)[["well", "tile", "cell", "count"]]
+            .rename(columns={"count": UMI_COUNT_1})
+            .set_index(cols),
+            on=cols,
+        )
+        .join(s["count"].sum().rename(UMI_COUNT), on=cols)
+        .assign(
+            **{
+                UMI_COUNT_0: lambda x: x[UMI_COUNT_0].fillna(0).astype(int),
+                UMI_COUNT_1: lambda x: x[UMI_COUNT_1].fillna(0).astype(int),
+            }
+        )
+        .drop_duplicates(cols)
+        .drop([READ, BARCODE], axis=1)  # drop the read
+        .drop([POSITION_I, POSITION_J], axis=1)  # drop the read coordinates
+        .filter(regex="^(?!Q_)")  # remove read quality scores
+        .query("cell > 0")  # remove reads not in a cell
+    )
+    
+    cols_to_use = list(df_cells_UMI.columns.difference(df_cells.columns))
+    
+    return df_cells.merge(df_cells_UMI[cols_to_use + cols], left_on=cols, right_on=cols, how="inner")

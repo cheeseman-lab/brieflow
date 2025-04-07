56fa38484ec3e56bec4390aab4aaa13d8e7e1ca4
Roshan Kern <rsk119@case.edu>
2025-04-07 16:51:24 -0400

    use md

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'brieflow'
 copyright = '2025, Matteo Di Bernardo, Roshan Kern'
@@ -12,22 +8,25 @@ author = 'Matteo Di Bernardo, Roshan Kern'
 release = '4/8/2025'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    "autoapi.extension",
+    'myst_parser',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Use Markdown as source format
+source_suffix = {
+    '.md': 'markdown',
+}
 
+root_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,27 @@
+# Welcome to Brieflow
+
+```{image} ../../images/brieflow_logo.png
+:width: 200px
+:height: 200px
+:align: center
+:alt: Brieflow logo
+```
+
+Brieflow is a pipeline written in [Snakemake](http://snakemake.readthedocs.io) that simplifies and automates many of the steps in optical pooled screen (OPS) analysis.  
+Although written to be easily deployed on a Slurm cluster, it can also be run on other cloud-based or local systems.  
+Brieflow is a community-driven project, and we welcome contributions from anyone interested in improving the tool.
+
+Brieflow currently automates the following OPS tasks:
+
+- Preprocessing
+
+To get started, see the [Quickstart](quickstart.md)!
+
+```{toctree}
+:hidden:
+:maxdepth: 2
+:caption: Contents:
+
+quickstart.md
+dev.md
+```

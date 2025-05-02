b53c835fe4d31236daa9d8bb767af614e11a05f3
Alexa Mallar <alexamallar@gmail.com>
2025-05-02 13:55:12 -0400

    cp-measure scripts and option functionality (#97)
    * cp-measure scripts and option functionality
    
    * remove unecessary cp_measure script
    
    * cp_measure lib: combined area and single obj helper fn & modified mask-mask pairs
    
    * format fix
    
    * Add Integration tests past merge (#100)
    
    * add hash change
    
    * allow tile subset
    
    * separate montage and aggregate targets
    
    * remove print statements
    
    * add small test analysis files
    
    * add nb
    
    * remove nb
    
    * clarify scripts
    
    * update integration test
    
    * update pytest
    
    * update caching
    
    * cache test
    
    * just run preprocess
    
    * cache test
    
    * remove snakemake cache
    
    * added CP-METHOD param to config/config.yml
    
    * Trigger RTD rebuild
    
    * small fixes to config & cpmeasure
    
    ---------
    
    Co-authored-by: Roshan Kern <105665675+roshankern@users.noreply.github.com>

--- workflow/scripts/phenotype/extract_phenotype_cp_multichannel.py
+++ workflow/scripts/phenotype/extract_phenotype_cp_multichannel.py
@@ -4,22 +4,34 @@ from lib.phenotype.extract_phenotype_cp_multichannel import (
     extract_phenotype_cp_multichannel,
 )
 
+from lib.phenotype.extract_phenotype_cp_measure import extract_phenotype_cp_measure
+
 # load inputs
 data_phenotype = imread(snakemake.input[0])
 nuclei = imread(snakemake.input[1])
 cells = imread(snakemake.input[2])
 cytoplasms = imread(snakemake.input[3])
 
-# extract phenotype CellProfiler information
-phenotype_cp = extract_phenotype_cp_multichannel(
-    data_phenotype=data_phenotype,
-    nuclei=nuclei,
-    cells=cells,
-    cytoplasms=cytoplasms,
-    foci_channel=snakemake.params.foci_channel,
-    channel_names=snakemake.params.channel_names,
-    wildcards=snakemake.wildcards,
-)
+if snakemake.params.cp_method == "cp_measure":
+    # Extract features using cp_measure
+    phenotype_cp = extract_phenotype_cp_measure(
+        data_phenotype=data_phenotype,
+        nuclei=nuclei,
+        cells=cells,
+        cytoplasms=cytoplasms,
+        channel_names=snakemake.params.channel_names,
+    )
+else:
+    # extract phenotype CellProfiler information
+    phenotype_cp = extract_phenotype_cp_multichannel(
+        data_phenotype=data_phenotype,
+        nuclei=nuclei,
+        cells=cells,
+        cytoplasms=cytoplasms,
+        foci_channel=snakemake.params.foci_channel,
+        channel_names=snakemake.params.channel_names,
+        wildcards=snakemake.wildcards,
+    )
 
 # save phenotype cp
 phenotype_cp.to_csv(snakemake.output[0], index=False, sep="\t")

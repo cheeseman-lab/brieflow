9d81b1259ca4b112d4ce9d759f8e68052c6a9872
amallar <alexamallar@gmail.com>
2025-05-05 14:20:43 -0400

    conditional imports in cp_multichannel script + cpmeasure conda env setup instructions

--- workflow/scripts/phenotype/extract_phenotype_cp_multichannel.py
+++ workflow/scripts/phenotype/extract_phenotype_cp_multichannel.py
@@ -1,11 +1,5 @@
 from tifffile import imread
 
-from lib.phenotype.extract_phenotype_cp_multichannel import (
-    extract_phenotype_cp_multichannel,
-)
-
-from lib.phenotype.extract_phenotype_cp_measure import extract_phenotype_cp_measure
-
 # load inputs
 data_phenotype = imread(snakemake.input[0])
 nuclei = imread(snakemake.input[1])
@@ -13,7 +7,21 @@ cells = imread(snakemake.input[2])
 cytoplasms = imread(snakemake.input[3])
 
 if snakemake.params.cp_method == "cp_measure":
-    # Extract features using cp_measure
+    from lib.phenotype.extract_phenotype_cp_measure import (
+        extract_phenotype_cp_measure,
+    )
+    # TO-DO: Ensure conda environment is set up for cp_measure when using this method.
+    # A quick guide:
+    # 1. Clone brieflow environment:
+    #    conda create --name brieflow_cpmeasure_env --clone brieflow_main_env
+    # 2. Activate the environment:
+    #    conda activate brieflow_cpmeasure_env
+    # 3. Install the required package:
+    #    pip install cp-measure
+    # 4. Verify dependencies with 'conda list'- Cpmeasure requires Python 3.8 or later, and the following package versions:
+    #    - NumPy 1.24.3*
+    #    - centrosome 1.3.0*
+    #    If you have issues running cpmeasure, you may need to downgrade these packages in the cloned environment.
     phenotype_cp = extract_phenotype_cp_measure(
         data_phenotype=data_phenotype,
         nuclei=nuclei,
@@ -22,6 +30,10 @@ if snakemake.params.cp_method == "cp_measure":
         channel_names=snakemake.params.channel_names,
     )
 else:
+    from lib.phenotype.extract_phenotype_cp_multichannel import (
+        extract_phenotype_cp_multichannel,
+    )
+
     # extract phenotype CellProfiler information
     phenotype_cp = extract_phenotype_cp_multichannel(
         data_phenotype=data_phenotype,

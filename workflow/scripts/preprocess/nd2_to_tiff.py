f08b635047917c08cd4961b46345d1ee424cab93
acepedadiaz <acepedadiaz@fry.wi.mit.edu>
2025-05-23 15:13:14 -0400

    Ruff check nd2 to tiff snakemake integration

--- workflow/scripts/preprocess/nd2_to_tiff.py
+++ workflow/scripts/preprocess/nd2_to_tiff.py
@@ -4,8 +4,8 @@ from lib.preprocess.preprocess import nd2_to_tiff
 
 # convert the ND2 file to a TIF image array
 image_array = nd2_to_tiff(
-    snakemake.input, 
-    channel_order_flip=snakemake.params.channel_order_flip, 
+    snakemake.input,
+    channel_order_flip=snakemake.params.channel_order_flip,
     best_focus_channel=snakemake.params.best_focus_channel,
     z_handling=snakemake.params.z_handling,
 )

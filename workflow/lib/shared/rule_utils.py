129bce33d5867418a55a8f299593746fa6214fbe
mdiberna <mdiberna@fry.wi.mit.edu>
2025-07-24 17:39:22 -0400

    revert rule utils

--- workflow/lib/shared/rule_utils.py
+++ workflow/lib/shared/rule_utils.py
@@ -55,20 +55,6 @@ def get_alignment_params(wildcards, config):
 
         return alignment_params
 
-        # Add custom alignment parameters if they exist
-        if "custom_align" in plate_config:
-            alignment_params["custom_align"] = True
-            alignment_params["custom_channels"] = plate_config.get(
-                "custom_channels", []
-            )
-            alignment_params["custom_offset_yx"] = plate_config.get(
-                "custom_offset_yx", (0, 0)
-            )
-        else:
-            alignment_params["custom_align"] = False
-
-        return alignment_params
-
     # If no plate-specific alignments, use global config
     base_params = {
         "align": config["phenotype"].get("align", False),
@@ -78,18 +64,6 @@ def get_alignment_params(wildcards, config):
         "riders": config["phenotype"].get("riders", []),
         "remove_channel": config["phenotype"].get("remove_channel", False),
     }
-
-    # Add global custom alignment parameters if they exist
-    if "custom_align" in config["phenotype"]:
-        base_params["custom_align"] = config["phenotype"].get("custom_align", False)
-        base_params["custom_channels"] = config["phenotype"].get("custom_channels", [])
-        base_params["custom_offset_yx"] = config["phenotype"].get(
-            "custom_offset_yx", (0, 0)
-        )
-    else:
-        base_params["custom_align"] = False
-
-    return base_params
 
     # Add global custom alignment parameters if they exist
     if "custom_align" in config["phenotype"]:

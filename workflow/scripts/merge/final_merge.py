f69e294248043f94c08719fbb16988ed13d3e3e2
acepedadiaz <acepedadiaz@fry.wi.mit.edu>
2025-10-07 11:33:57 -0400

    bugfix renaming coordinate cols

--- workflow/scripts/merge/final_merge.py
+++ workflow/scripts/merge/final_merge.py
@@ -5,6 +5,9 @@ from lib.shared.file_utils import validate_dtypes
 # Load deduplicated merge data
 merge_deduplicated = validate_dtypes(pd.read_parquet(snakemake.input[0]))
 
+# Extract configuration parameters
+approach = getattr(snakemake.params, "approach", "fast")
+
 # Load full feature data
 cp_phenotype = validate_dtypes(pd.read_parquet(snakemake.input[1]))
 
@@ -15,22 +18,16 @@ merged_final = merge_deduplicated.merge(
     on=["plate", "well", "tile", "cell_0"],
 )
 
-# Rename coordinate columns to global naming convention
-coordinate_rename_map = {
-    "i_0": "global_i_0",
-    "j_0": "global_j_0",
-    "i_1": "global_i_1",
-    "j_1": "global_j_1",
-}
-
-# Only rename columns that exist in the dataframe
-existing_columns = merged_final.columns.tolist()
-rename_map = {
-    old: new for old, new in coordinate_rename_map.items() if old in existing_columns
-}
-
-if rename_map:
-    merged_final = merged_final.rename(columns=rename_map)
+# Rename coordinate columns to global naming convention only for stitch approach
+if snakemake.params.approach == "stitch":
+    merged_final = merged_final.rename(
+        columns={
+            "i_0": "global_i_0",
+            "j_0": "global_j_0",
+            "i_1": "global_i_1",
+            "j_1": "global_j_1",
+        }
+    )
 
 # Save final merged dataset
 merged_final.to_parquet(snakemake.output[0])

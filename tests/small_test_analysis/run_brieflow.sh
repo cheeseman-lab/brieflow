4e98d62ca02f3d2be52828bcba16684c2cb1607a
matteo <md3498@columbia.edu>
2025-10-19 12:26:53 +0000

    further windback

--- tests/small_test_analysis/run_brieflow.sh
+++ tests/small_test_analysis/run_brieflow.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # Run only the preprocess rules
-# snakemake --use-conda --cores all \
-#     --snakefile "../../workflow/Snakefile" \
-#     --configfile "config/config.yml" \
-#     --rerun-triggers mtime \
-#     --until all_preprocess # all_sbs all_phenotype all_merge all_aggregate all_cluster
-
-snakemake --executor googlebatch \
-    --workflow-profile "google_batch/" \
+snakemake --use-conda --cores all \
     --snakefile "../../workflow/Snakefile" \
     --configfile "config/config.yml" \
     --rerun-triggers mtime \

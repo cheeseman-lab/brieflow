1764cbbed79524e1976bba512facbefc817ee3a6
acepedadiaz <acepedadiaz@fry.wi.mit.edu>
2025-10-29 16:53:52 -0400

    restored to full test (previously truncated until merge)

--- tests/small_test_analysis/run_brieflow.sh
+++ tests/small_test_analysis/run_brieflow.sh
@@ -5,4 +5,4 @@ snakemake --use-conda --cores all \
     --snakefile "../../workflow/Snakefile" \
     --configfile "config/config.yml" \
     --rerun-triggers mtime \
-    --until all_preprocess all_sbs all_phenotype all_merge
+    --until all_preprocess all_sbs all_phenotype all_merge all_aggregate all_cluster

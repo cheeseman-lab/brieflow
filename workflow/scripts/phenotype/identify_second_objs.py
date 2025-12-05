417293448b94fa75fa6265f7acf0a95e1dce6816
acepedadiaz <acepedadiaz@fry.wi.mit.edu>
2025-12-05 14:03:09 -0500

    updated parameters and added scaffold for ML-based segmentation

--- workflow/scripts/phenotype/identify_second_objs.py
+++ workflow/scripts/phenotype/identify_second_objs.py
@@ -1,8 +1,5 @@
 from tifffile import imread, imwrite
 import pandas as pd
-import pickle
-
-from lib.phenotype.segment_secondary_object import segment_second_objs
 
 # Load input files
 data_phenotype = imread(snakemake.input[0])
@@ -21,31 +18,71 @@ if "i" in phenotype_info.columns and "j" in phenotype_info.columns:
         for idx, row in phenotype_info.iterrows()
     }
 
-# Segment secondary objects
-second_obj_masks, cell_second_obj_table, updated_cytoplasm_masks = segment_second_objs(
-    image=data_phenotype,
-    second_obj_channel_index=snakemake.params.second_obj_channel_index,
-    cell_masks=cells,
-    cytoplasm_masks=cytoplasms,
-    second_obj_min_size=snakemake.params.second_obj_min_size,
-    second_obj_max_size=snakemake.params.second_obj_max_size,
-    size_filter_method=snakemake.params.size_filter_method,
-    threshold_smoothing_scale=snakemake.params.threshold_smoothing_scale,
-    threshold_method=snakemake.params.threshold_method,
-    use_morphological_opening=snakemake.params.use_morphological_opening,
-    opening_disk_radius=snakemake.params.opening_disk_radius,
-    fill_holes=snakemake.params.fill_holes,
-    declump_method=snakemake.params.declump_method,
-    declump_mode=snakemake.params.declump_mode,
-    suppress_local_maxima=snakemake.params.suppress_local_maxima,
-    maxima_reduction_factor=snakemake.params.maxima_reduction_factor,
-    use_shape_refinement=snakemake.params.use_shape_refinement,
-    proportion_threshold=snakemake.params.proportion_threshold,
-    max_objects_per_cell=snakemake.params.max_objects_per_cell,
-    overlap_threshold=snakemake.params.overlap_threshold,
-    nuclei_centroids=nuclei_centroids_dict,
-    max_total_objects=snakemake.params.max_total_objects,
-)
+# Get parameters from config
+params = snakemake.params.second_obj_params
+
+# Check which segmentation method to use
+use_ml = params.get("use_ml_segmentation", False)
+
+# Common parameters shared by both methods
+common_params = {
+    "image": data_phenotype,
+    "second_obj_channel_index": params["second_obj_channel_index"],
+    "cell_masks": cells,
+    "cytoplasm_masks": cytoplasms,
+    "second_obj_min_size": params.get("second_obj_min_size", 10),
+    "second_obj_max_size": params.get("second_obj_max_size", 200),
+    "size_filter_method": params.get("size_filter_method", "feret"),
+    "max_objects_per_cell": params.get("max_objects_per_cell", 120),
+    "overlap_threshold": params.get("overlap_threshold", 0.1),
+    "nuclei_centroids": nuclei_centroids_dict,
+    "max_total_objects": params.get("max_total_objects", 1000),
+}
+
+if use_ml:
+    from lib.phenotype.segment_secondary_object import segment_second_objs_ml
+
+    # Collect ML-specific parameters (any param not in common_params or standard CV params)
+    cv_only_params = {
+        "threshold_smoothing_scale", "threshold_method", "use_morphological_opening",
+        "opening_disk_radius", "fill_holes", "declump_method", "declump_mode",
+        "suppress_local_maxima", "maxima_reduction_factor", "use_shape_refinement",
+        "proportion_threshold", "use_ml_segmentation", "second_obj_channel_index"
+    }
+
+    ml_params = {
+        k: v for k, v in params.items()
+        if k not in common_params and k not in cv_only_params
+    }
+
+    # Call ML segmentation with common params and ML-specific params
+    second_obj_masks, cell_second_obj_table, updated_cytoplasm_masks = segment_second_objs_ml(
+        **common_params,
+        **ml_params
+    )
+else:
+    from lib.phenotype.segment_secondary_object import segment_second_objs
+
+    # CV-specific parameters with defaults
+    cv_params = {
+        "threshold_smoothing_scale": params.get("threshold_smoothing_scale", 1.3488),
+        "threshold_method": params.get("threshold_method", "otsu_two_peak"),
+        "use_morphological_opening": params.get("use_morphological_opening", True),
+        "opening_disk_radius": params.get("opening_disk_radius", 1),
+        "fill_holes": params.get("fill_holes", "both"),
+        "declump_method": params.get("declump_method", "shape"),
+        "declump_mode": params.get("declump_mode", "watershed"),
+        "suppress_local_maxima": params.get("suppress_local_maxima", 20),
+        "maxima_reduction_factor": params.get("maxima_reduction_factor", None),
+        "use_shape_refinement": params.get("use_shape_refinement", False),
+        "proportion_threshold": params.get("proportion_threshold", 0.4),
+    }
+
+    # Call traditional segmentation
+    second_obj_masks, cell_second_obj_table, updated_cytoplasm_masks = segment_second_objs(
+        **common_params,
+        **cv_params
+    )
 
 # Save outputs
 # Save secondary object masks as TIFF
